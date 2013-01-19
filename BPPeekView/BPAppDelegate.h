af30a73fd32a0ab31537cb853872ef76e262a291
Bojan Percevic <bojanpercevic@Bojans-MacBook-Pro.local>
2013-01-19 00:01:42 -0500

    Initial Commit

---
+++ BPPeekView/BPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BPAppDelegate.h
+//  BPPeekView
+//
+//  Created by Bojan Percevic on 1/19/13.
+//  Copyright (c) 2013 Bojan Percevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BPPeekView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BPPeekView
+//
+//  Created by Bojan Percevic on 1/19/13.
+//  Copyright (c) 2013 Bojan Percevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BPAppDelegate class]));
+    }
+}
